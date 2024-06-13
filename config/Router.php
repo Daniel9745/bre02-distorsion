92d953355784f6323ff13d8cb442f05000f9447e
Daniel9745 <danielnoroasy@yahoo.fr>
2024-06-13 11:37:10 +0200

    projet distorsion

---
+++ config/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+class Router{
+
+    public function __construct(){
+
+    }
+    public function handleRequest(string $route): void{
+        if($route !== null && $route === "home"){
+
+        }
+        elseif($route !== null && $route === "add-category"){
+
+        }
+        elseif($route !== null && $route === "add-room"){
+
+        }
+        elseif($route !== null && $route === "add-message"){
+
+        }
+        elseif($route !== null && $route === "show-room"){
+
+        }
+        elseif($route !== null){
+
+        }
+        else{
+            
+        }
+    }
+}
